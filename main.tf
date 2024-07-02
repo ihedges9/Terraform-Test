3602cc990d555b44adb403414a00aacf52e651f6
ihedges9 <42546907+ihedges9@users.noreply.github.com>
2024-07-02 11:37:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 provider "aws" {
-  region = var.region
+  region = us-east2
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
+    name   = "terraform-test"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
