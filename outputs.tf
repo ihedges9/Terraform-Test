d68c295c893588096c49dca25f0730c0d997ac9d
ihedges9 <42546907+ihedges9@users.noreply.github.com>
2024-07-02 12:37:59 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-#output "instance_ami" {
-#  value = aws_instance.windows.ami
-#}
+output "instance_ami" {
+  value = aws_instance.app_server.ami
+}
 
 #output "instance_arn" {
 #  value = aws_instance.windows.arn
