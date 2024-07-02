053b7ae7fb64cf7181a4c3a417a170d5478767bd
ihedges9 <42546907+ihedges9@users.noreply.github.com>
2024-07-02 12:43:20 -0400

    Update main.tf to spin two instances

--- main.tf
+++ main.tf
@@ -7,6 +7,15 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "AppServerInstance"
   }
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0f811217032cd31f7"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "WebServerInstance"
+  }
+
 }
