f0c4fabeb56dac4d72bb0210129d7d7177695a55
ihedges9 <42546907+ihedges9@users.noreply.github.com>
2024-07-02 14:09:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_instance" "app_server" {
+-resource "aws_instance" "app_server" {
   ami           = "ami-024ebc7de0fc64e44"
   instance_type = "t2.micro"
 
@@ -11,7 +11,7 @@ resource "aws_instance" "app_server" {
   }
 }
 
-resource "aws_instance" "web_server" {
+-resource "aws_instance" "web_server" {
   ami           = "ami-024ebc7de0fc64e44"
   instance_type = "t2.micro"
 
